6e549d77f96bbe325b836d17ced501b3b5e7c7d8
Francisco Baena <yo@baenafrancisco.com>
2013-10-05 12:34:34 +0200

    Añadida página de inicio

--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Cumpleaños de Paula</title>
+	<title>Tarantino Temed Party</title>
+	<link rel="stylesheet" type="text/css" href="./css/pulpFiction.css">
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
 </head>
-<body>
-	
+<body id="main">
+	<form id="login" action="./login">
+		<input type="text" name="user" placeholder="user"><br>
+		<input type="password" name="password" placeholder="password">
+	</form>
+	<div id="pulp-intro"></div>
 </body>
 </html>
